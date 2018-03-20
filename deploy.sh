4f3b39af1140225fff127bbc31991461a37580f5
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2018-03-20 12:29:00 +0100

    add files for docker installation

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo deploying Populatie Edit Tool...
+
+export EDIT_TOOL_VERSION=0.1.0
+
+export DOMAIN_PREFIX=pop-edit-tool
+export DOMAIN_SUFFIX=local
+
+COMPOSE_ARGS="\
+    -p pop \
+    -f populatie-edit-tool.yml"
+
+docker-compose \
+    $COMPOSE_ARGS \
+    build
+
+docker-compose \
+    $COMPOSE_ARGS \
+    up -d
