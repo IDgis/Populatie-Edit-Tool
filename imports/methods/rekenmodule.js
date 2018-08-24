42626758b3f014abe9cefc2b99beb8e43df2d759
Kevin van den Bosch <kevin.van.den.bosch@idgis.nl>
2018-08-24 15:07:12 +0200

    changed the way the number of decimals are returned and the maatwerk option

--- imports/methods/rekenmodule.js
+++ imports/methods/rekenmodule.js
@@ -50,10 +50,7 @@ function calculateVerblijfsfuncties(verblijfsobject, table, huishoudensgrootte) 
             const hoofdfunctie = hoofdfunctieObject['hoofdfunctie BAG'];
             const aanvullendeIndelingen = hoofdfunctieObject['aanvullende functies'];
             const aanvullendeIndeling = verblijfsfunctie['aanvullend'] === '' ? aanvullendeIndelingen[0]['functie'] : verblijfsfunctie['aanvullend'];
-            const rekenIndicator = hoofdfunctieObject['rekenindicator'];
-            const oppervlakte = verblijfsfunctie['Oppervlakte']
-            const rekenObject = aanvullendeIndelingen.filter(indeling => indeling['functie'] === aanvullendeIndeling)[0];
-            const numPersons = calculateNumPersons(rekenIndicator, oppervlakte, rekenObject, verblijfsobject, huishoudensgrootte);
+            const numPersons = calculateNumPersons(verblijfsfunctie, huishoudensgrootte, hoofdfunctieObject);
 
             if(verblijfsfunctie['aantal-personen'] !== parseFloat(numPersons) &&
                     !(verblijfsfunctie.mutatie && verblijfsfunctie.mutatie === 'toegevoegd')) {
@@ -82,35 +79,36 @@ function getHoofdfunctieObject(verblijfsfunctie, table) {
 /**
  * Calculates the number of persons
  * 
- * @param {String} rekenindicator Defines the way the number of persons should be calculated by either "personen per vbo" or "m2 vbo per persoon"
- * @param {Number} opp The oppervlakte of the verblijfsfunctie
- * @param {Object} indelingObject The aanvullende indelingen object from the BAG data tabel
- * @param {Object} verblijfsobject The Verblijfsobject that contains the Verblijfsfunctie to be calculated
+ * @param {Object} verblijfsfunctie The verblijfsfunctie of which the number of persons are calculated
  * @param {Number} huishoudensgrootte The huishoudensgrootte to calculate
+ * @param {Object} hoofdfunctieBag The hoofdfunctie BAG from the rekentabel
  */
-function calculateNumPersons(rekenindicator, opp, indelingObject, verblijfsobject, huishoudensgrootte) {
-    const aantal = indelingObject['aantal'];
+function calculateNumPersons(verblijfsfunctie, huishoudensgrootte, hoofdfunctieBag) {
+    const defaultAantal = verblijfsfunctie['aantal-personen'];
+    const aanvullendeIndelingenBag = hoofdfunctieBag['aanvullende functies'];
+    const aanvullendeIndelingBag = verblijfsfunctie.aanvullend === '' ? aanvullendeIndelingenBag[0]['functie'] : verblijfsfunctie.aanvullend;
+    const rekenindicator = hoofdfunctieBag.rekenindicator;
+    const oppervlakte = verblijfsfunctie.Oppervlakte;
+    const aanvullendeIndelingObject = aanvullendeIndelingenBag.filter(indeling => indeling.functie === aanvullendeIndelingBag)[0];
+    const rekenfactor = aanvullendeIndelingObject.aantal;
 
-    if(indelingObject["functie"] === "Woonfunctie gezin" && huishoudensgrootte > 0.0) {
+    if (aanvullendeIndelingObject.functie === "Woonfunctie gezin" && huishoudensgrootte > 0.0) {
         return huishoudensgrootte;
-    } else if(rekenindicator === "personen per vbo" && typeof aantal === 'number') {
-        return aantal;
-    } else if(rekenindicator === "m2 vbo per persoon" && typeof aantal === 'number') {
-        return (opp / aantal).toFixed(2);
+    } else if (rekenindicator === "personen per vbo" && typeof rekenfactor === 'number') {
+        return rekenfactor;
+    } else if (rekenindicator === "m2 vbo per persoon" && typeof rekenfactor === 'number') {
+        let numDecimals = 2;
+        if (defaultAantal > 0.0) {
+            numDecimals = defaultAantal.toString().split('.')[1].length;
+        }
+        
+        return (oppervlakte / rekenfactor).toFixed(numDecimals);
     } else {
-        const straatnaam = verblijfsobject['Adres']['straat'];
-        const nr = verblijfsobject['Adres']['huisnummer'];
-        const letter = verblijfsobject['Adres']['huisletter'];
-        const toevoeging = verblijfsobject['Adres']['huisnummer-toevoeging'];
-        const postcode = verblijfsobject['Adres']['postcode'];
-        const woonplaats = verblijfsobject['Adres']['woonplaats'];
-
-        const adres = `${straatnaam} ${nr}${letter}${toevoeging}, ${postcode} ${woonplaats}`;
-        const functie = indelingObject['functie'];
-        const maatwerkText = `Maatwerk voor: ${adres}. \nVerblijfsfunctie: ${functie}. \n\nVul hier handmatig het aantal personen in voor het bvo van ${opp}m2!`;
-        const maatwerk = prompt(maatwerkText);
-        return maatwerk ? maatwerk : 0;
+        // Maatwerk, return default aantal-personen. Edit in Verblijfsfunctie itself
+        return defaultAantal;
     }
+
+   return 0;
 }
 
 /**
